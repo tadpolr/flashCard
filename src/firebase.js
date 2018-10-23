5a6da23fb6e6605871ab2f1f38658d02c76f5b5a
Tadpol Rachatasumrit <tadpolr@trmbp.local>
2018-10-24 00:58:52 +0700

    Fixed config

--- src/firebase.js
+++ src/firebase.js
@@ -3,11 +3,20 @@ import firebase from 'firebase';
 var config = {
   apiKey: 'AIzaSyCdQahZea5pcVr5r43GIZgRxaM3UeveLuE',
   authDomain: 'flashcard-c18ba.firebaseapp.com',
-  databaseURL: 'https://flashcard-c18ba.firebaseio.com/',
+  databaseURL: 'https://flashcard-c18ba.firebaseio.com',
   projectId: 'flashcard-c18ba',
-  storageBucket: 'gs://flashcard-c18ba.appspot.com',
-  messagingSenderId: '<SENDER_ID>',
+  storageBucket: 'flashcard-c18ba.appspot.com',
+  messagingSenderId: '870748163041',
 };
+
+// var config = {
+//   apiKey: 'AIzaSyCdQahZea5pcVr5r43GIZgRxaM3UeveLuE',
+//   authDomain: 'flashcard-c18ba.firebaseapp.com',
+//   databaseURL: 'https://flashcard-c18ba.firebaseio.com/',
+//   projectId: 'flashcard-c18ba',
+//   storageBucket: 'gs://flashcard-c18ba.appspot.com',
+//   messagingSenderId: '870748163041',
+// };
 var fire = firebase.initializeApp(config);
 
 // Initialize Cloud Firestore through Firebase
